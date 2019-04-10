03b97e1fc8c05d9efc8aafce18528eae5ae398c4
武尾　洋彰 <192x011x@istc-a-070.epc.center.kobe-u.ac.jp>
2019-04-10 14:41:39 +0900

    update Vec3.js

--- w02/Vec3.js
+++ w02/Vec3.js
@@ -17,3 +17,134 @@ Vec3.prototype.sum = function()
 {
     return this.x + this.y + this.z;
 }
+
+Vec3.prototype.min = function()
+{
+    if (this.x < this.y)
+    {
+        if(this.x <= this.z)
+        {
+            return this.x;
+        }
+        else
+        {
+            return this.z;
+        }
+    }
+
+    if (this.y < this.z)
+    {
+        if (this.y <= this.x)
+        {
+            return this.y;
+        }
+        else
+        {
+            return this.x;
+        }
+    }
+
+    if (this.z < this.x)
+    {
+        if (this.z <= this.y)
+        {
+            return this.z;
+        }
+        else
+        {
+            return this.y;
+        }
+    }
+}
+
+Vec3.prototype.max = function()
+{
+    if (this.x < this.y)
+    {
+        if(this.x >= this.z)
+        {
+            return this.x;
+        }
+        else if(this.z < this.y)
+        {
+            return this.z;
+        }
+        else
+        {
+            return this.y;
+        }
+    }
+
+    if (this.y < this.z)
+    {
+        if (this.y >= this.x)
+        {
+            return this.y;
+        }
+        else if(this.x < this.z)
+        {
+            return this.x;
+        }
+        else
+        {
+            return this.z;
+        }
+    }
+
+    if (this.z < this.x)
+    {
+        if (this.z >= this.y)
+        {
+            return this.z;
+        }
+        else if(this.y < this.x)
+        {
+            return this.y;
+        }
+        else
+        {
+            return this.x;
+        }
+    }
+
+}
+
+
+Vec3.prototype.max = function()
+{
+    if (this.x > this.y)
+    {
+        if(this.x >= this.z)
+        {
+                return this.x;
+            }
+            else
+            {
+                return this.z;
+            }
+        }
+    
+        if (this.y > this.z)
+        {
+            if (this.y >= this.x)
+            {
+                return this.y;
+            }
+            else
+            {
+                return this.x;
+            }
+        }
+    
+        if (this.z > this.x)
+        {
+            if (this.z >= this.y)
+            {
+                return this.z;
+            }
+            else
+            {
+                return this.y;
+            }
+        }
+}
